e439feea18d7c9b47db3dacb86a3710526e01e10
cbozay <cbozay90@hotmail.com>
2022-08-24 22:22:58 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import City from "./City";
 function App() {
+  const key = "84a91b51124f2bc14e2104a40efa11c9";
+  const [search, setSearch] = useState("elazığ");
+  const [city, setCity] = useState();
+  useEffect(() => {
+    async function getApi() {
+      try {
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}&units=metric`
+        );
+        setCity(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getApi();
+  }, [search]);
+  console.log(search);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          placeholder="Enter a City"
+          style={{
+            border: "1px solid",
+            padding: "5px",
+            marginTop: "10px",
+            borderRadius: "5px",
+          }}
+        />
+        {city && <City city={city} />}
+      </div>
     </div>
   );
 }
